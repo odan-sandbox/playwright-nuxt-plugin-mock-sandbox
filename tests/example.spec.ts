6b7f8d8bb7b45ae7c83f0da387a47f49e6668ca9
odan <odan3240@gmail.com>
2022-04-19 01:46:30 +0900

    直接代入で動く

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,12 +2,18 @@ import { test, expect } from '@playwright/test'
 
 test('basic test', async ({ page }) => {
   await page.goto('http://localhost:3000/')
+  await page.waitForFunction('!!window.$nuxt')
+
   const text1 = page.locator('[data-testid="text"]')
   await expect(text1).toHaveText('init')
+
+  await page.evaluate(() => {
+    window.$nuxt.$user.login = () => 'updated'
+  })
 
   const button = page.locator('[data-testid="button"]')
   await button.click()
 
   const text2 = page.locator('[data-testid="text"]')
-  await expect(text2).toHaveText('hello')
+  await expect(text2).toHaveText('updated')
 })
